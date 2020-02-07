9ca7ef00be30b73e7a4fdc70469443916cb7721d
SET001 <settydark@gmail.com>
2020-02-07 22:24:27 +0200

    naive implementation of fullscreen with max resolution

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use ggez;
 use ggez::event;
 use ggez::graphics;
 use ggez::nalgebra::Point2;
-
+use ggez::conf;
 use std::path;
 use std::env;
 
@@ -15,6 +15,7 @@ use crate::state::MainState;
 
 
 fn main() -> GameResult{
+  
   let resource_dir = if let Ok(manifest_dir) = env::var("CARGO_MANIFEST_DIR") {
     let mut path = path::PathBuf::from(manifest_dir);
     path.push("resources");
@@ -25,6 +26,13 @@ fn main() -> GameResult{
 
   let cb = ggez::ContextBuilder::new("Last Day", "ggez").add_resource_path(resource_dir);
   let (ctx, event_loop) = &mut cb.build()?;
+
+  //  TODO: move this to config
+  let screen_width = 2560.0;
+  let screen_height = 1080.0;
+  graphics::set_mode(ctx, conf::WindowMode::default().dimensions(screen_width, screen_height)).unwrap();
+  graphics::set_screen_coordinates(ctx, graphics::Rect::new(0.0, 0.0, screen_width, screen_height)).unwrap();
+  ggez::graphics::set_fullscreen(ctx, conf::FullscreenType::Desktop)?;
 
   let font = graphics::Font::new(ctx, "/DejaVuSerif.ttf").unwrap();
   let state = &mut MainState::new(ctx, font);
