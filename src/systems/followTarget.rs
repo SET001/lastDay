c91087c0d1f858e06ec6bd14bceadb6c97fcce3b
SET001 <settydark@gmail.com>
2020-02-12 09:30:15 +0200

    adjust rotation when following

--- src/systems/followTarget.rs
+++ src/systems/followTarget.rs
@@ -6,6 +6,7 @@ pub struct FollowTarget;
 impl<'a> System<'a> for FollowTarget {
   type SystemData = (
     ReadStorage<'a, Position>,
+    WriteStorage<'a, RotationComponent>,
     ReadStorage<'a, TargetComponent>,
     WriteStorage<'a, LinearMovementComponent>,
     Entities<'a>,
@@ -13,19 +14,23 @@ impl<'a> System<'a> for FollowTarget {
   );
 
   
-  fn run(&mut self, (positions, targets, mut movements, entities, updater): Self::SystemData) {
+  fn run(&mut self, (positions, mut rotations, targets, mut movements, entities, updater): Self::SystemData) {
     use specs::Join;
-    for (position, target, entity, ()) in (&positions, &targets, &entities, !&movements).join() {
+    for (position, rotation, target, entity, ()) in (&positions, &mut rotations, &targets, &entities, !&movements).join() {
       if let Some(targetPosition) = positions.get(target.0) {
+        let direction = (targetPosition.y - position.y).atan2(targetPosition.x - position.x);
+        rotation.0 = direction; 
         updater.insert(entity, LinearMovementComponent{
-          direction: (targetPosition.y - position.y).atan2(targetPosition.x - position.x),
+          direction,
           speed: 1.0  //  TODO: do not hardcode speed here
         })
       }
     }
-    for (position, target, entity, mut movement) in (&positions, &targets, &entities, &mut movements).join() {
+    for (position, rotation, target, movement) in (&positions, &mut rotations, &targets, &mut movements).join() {
       if let Some(targetPosition) = positions.get(target.0) {
-        movement.direction = (targetPosition.y - position.y).atan2(targetPosition.x - position.x);
+        let direction = (targetPosition.y - position.y).atan2(targetPosition.x - position.x);
+        rotation.0 = direction; 
+        movement.direction = direction;
       }
     }
   }
